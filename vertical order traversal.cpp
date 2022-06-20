fbcd12c736d5a0dc1e0313f354e0ff4aca12848d
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2022-06-21 00:09:46 +0530

    Update vertical order traversal.cpp

--- vertical order traversal.cpp
+++ vertical order traversal.cpp
@@ -1,102 +1,3 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-};
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-          
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-
-// Function for Inorder Traversal
-void printInorder(Node* root)
-{
-    if(!root)
-        return;
-        
-    printInorder(root->left);
-    cout<<root->data<<" ";
-    printInorder(root->right);
-}
-
-
- // } Driver Code Ends
 class Solution
 {
     public:
@@ -143,29 +44,49 @@ class Solution
     }
 };
 
-
-
-// { Driver Code Starts.
-int main() {
-    int t;
-    string  tc;
-    getline(cin,tc);
-    t=stoi(tc);
-    while(t--)
-    {
-        string s;
-        getline(cin,s);
-        // string c;
-        // getline(cin,c);
-        Solution obj;
-    	Node* root = buildTree(s);
-    	
-    	vector <int> res = obj.verticalOrder(root);
-    	for (int i : res) cout << i << " ";
-        cout << endl;
+// if we want to store the ans in 2d vector
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,vector<int> > > nodes;
+        queue<pair<TreeNode*,pair<int,int> > > q;
+        vector<vector<int>> ans;
+        
+        if(root==NULL){
+            return ans;
+        }
+        q.push(make_pair(root,make_pair(0,0)));
+        while(!q.empty()){
+            pair<TreeNode*,pair<int,int> > temp=q.front();
+            q.pop();
+            TreeNode* frontNode=temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+            
+            nodes[hd][lvl].push_back(frontNode->val);
+            
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
+            }
+            
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+            }
+            
+        }
+        for(auto i : nodes){
+            // i row representing <hd,map>
+            vector<int> v;
+            for(auto j : i.second){
+                sort(j.second.begin(),j.second.end());
+                for(auto k : j.second)
+                {
+                    v.push_back(k);
+                }
+            }
+            ans.push_back(v);
+        }
+        return ans;
     }
-	return 0;
-}
+};
 
-
-  // } Driver Code Ends
