4b6ac0f0725a77f3a5f41eb61ee4544df1da0db8
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2022-07-22 09:49:57 +0530

    Update sort colors.cpp

--- sort colors.cpp
+++ sort colors.cpp
@@ -40,3 +40,32 @@ public:
     }
     
 };
+
+// 3rd solution
+class Solution {
+public:
+//    using three loops we are going to found 0 then swap it with the starting number now increase.
+//     1 ki value khud arrange ho jayegi bcoz 0 se ist m replace kr diya and for 2 last m replace kr diya toh jo hojyega 1 apne ap...
+    void sortColors(vector<int>& nums) {
+        int i = 0; 
+        int j = 0;
+        int k = nums.size()-1;
+        while(j<=k){
+            if(nums[j] == 0){
+                swap(nums[i],nums[j]);
+                
+            i++;
+            j++;
+            }
+            else if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
+            }
+            else{
+                j++;
+            }
+        }
+        
+        
+    }
+};
