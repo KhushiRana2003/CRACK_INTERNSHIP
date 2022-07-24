d7b868ab562657ffb6f4ddca2bf0569d26b466c3
Khushi Rana <91715072+KhushiRana2003@users.noreply.github.com>
2022-07-24 10:28:15 +0530

    Create Search a 2D Matrix II.cpp

---
+++ Search a 2D Matrix II.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i = 0, j = matrix[0].size()-1; i < matrix.size() && j >= 0; ){
+            if(matrix[i][j] < target) i++;
+            else if (matrix[i][j] > target) j--;
+            else return true;
+        }
+        return false;
+    }
+};
